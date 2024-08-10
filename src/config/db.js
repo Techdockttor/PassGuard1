5ca87165b7fc1d8b926d0e5cb0c801cc1a77d72a
Melusi2022 <melusimedia@gmail.com>
2024-08-10 01:18:55 -0700

    DB

---
+++ src/config/db.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
+
+// Set strictQuery to suppress the warning
+mongoose.set('strictQuery', true);
+
+// Enable Mongoose debugging
+mongoose.set('debug', true);
+
+// Function to retrieve MongoDB URI from environment variables or use default
+function getMongoURI() {
+  const defaultURI = 'mongodb://localhost:27017/passguarddb';
+  return process.env.MONGODB_URI || defaultURI;
+}
+
+// Usage example:
+const mongoURI = getMongoURI();
+console.log('MongoURI:', mongoURI);
+
+// Set mongoose options and connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, // Added unified topology for better server discovery and monitoring
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('MongoDB connection is open');
+});
+
+module.exports = db;
