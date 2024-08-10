856d79e3037be510b1b068abf25327c4b2bb2404
Melusi2022 <melusimedia@gmail.com>
2024-08-10 01:10:29 -0700

    Password

---
+++ src/models/password.js
@@ -0,0 +1,20 @@
+// password.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { API_URL, KEY } = require('../controllers/config.js');
+const { AJAX } = require('../controllers/ajax.js');
+const mongoURI = 'mongodb://localhost:27017/passguard';
+
+const passwordSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date, required: true },
+    generatedPassword: { type: String, required: true }, 
+	status: { type: String, enum: ['active', 'expired'], default: 'active'}
+});
+
+const Password = mongoose.model('Password', passwordSchema);
+
+module.exports = Password;
+;
